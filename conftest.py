c7c145443b4aaf52b86a880306c67e242567c11c
Ksenia Fesik <ksenia.fesik@gmail.com>
2021-05-23 17:55:21 +0300

    maximied screen

--- conftest.py
+++ conftest.py
@@ -8,6 +8,7 @@ options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-setuid-sandbox')
+options.add_argument('--start-maximized')
 
 
 def pytest_addoption(parser):
